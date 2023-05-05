a3edeb6e9609cb3be0ee35999a47f93a43c9647c
Eric Bach <bach.eric@gmail.com>
2023-05-04 18:28:44 -0600

    Fix error message being static

--- frontend/src/pages/Admin/Schedule.tsx
+++ frontend/src/pages/Admin/Schedule.tsx
@@ -133,6 +133,7 @@ function Schedule() {
     }
 
     // Check for overlaps
+    setError(undefined);
     for (let j = 0; j < times.length - 1; j++) {
       if (times[j] > times[j + 1]) {
         setError('The schedule has overlapping time slots. Please correct before proceeding.');
@@ -162,12 +163,7 @@ function Schedule() {
       <Grid container spacing={{ md: 1, lg: 1 }} columns={{ md: 6, lg: 6 }}>
         <Grid md={2} lg={2}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar
-              value={date}
-              minDate={dayjs()}
-              maxDate={dayjs().add(1, 'year')}
-              onChange={(newDate) => dateSelected(newDate ?? dayjs())}
-            />
+            <DateCalendar value={date} minDate={dayjs()} maxDate={dayjs().add(1, 'year')} onChange={(newDate) => dateSelected(newDate ?? dayjs())} />
           </LocalizationProvider>
         </Grid>
 
@@ -230,11 +226,7 @@ function Schedule() {
                                         helperText={getIn(errors, `appointments.${index}.sk`)}
                                       />
                                       {/* TODO Field error/helperText does not work so using ErrorMessage with a custom styled component */}
-                                      <ErrorMessage
-                                        name={`appointments.${index}.sk`}
-                                        component={InvalidTimeComponent}
-                                        className='field-error'
-                                      />
+                                      <ErrorMessage name={`appointments.${index}.sk`} component={InvalidTimeComponent} className='field-error' />
                                     </LocalizationProvider>
                                   </Grid>
 
@@ -245,9 +237,7 @@ function Schedule() {
                                       value={values.appointments[index].duration}
                                       onChange={handleChange}
                                       onBlur={handleBlur}
-                                      error={
-                                        getIn(errors, `appointments.${index}.duration`) && getIn(touched, `appointments.${index}.duration`)
-                                      }
+                                      error={getIn(errors, `appointments.${index}.duration`) && getIn(touched, `appointments.${index}.duration`)}
                                       helperText={getIn(errors, `appointments.${index}.duration`)}
                                     />
                                   </Grid>
@@ -264,25 +254,14 @@ function Schedule() {
                                   </Grid>
 
                                   <Grid xs={2}>
-                                    <Button
-                                      color='error'
-                                      variant='contained'
-                                      onClick={() => removeField(values.appointments, index)}
-                                      sx={{ m: 1 }}
-                                    >
+                                    <Button color='error' variant='contained' onClick={() => removeField(values.appointments, index)} sx={{ m: 1 }}>
                                       X
                                     </Button>
                                   </Grid>
                                 </Grid>
                               </React.Fragment>
                             ))}
-                          <Button
-                            variant='contained'
-                            color='success'
-                            type='button'
-                            onClick={() => addField(values.appointments)}
-                            sx={{ m: 1 }}
-                          >
+                          <Button variant='contained' color='success' type='button' onClick={() => addField(values.appointments)} sx={{ m: 1 }}>
                             +
                           </Button>
                         </React.Fragment>
