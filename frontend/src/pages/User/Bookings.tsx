517af7ced541d0156165c012a8a90ac564b1b729
Eric Bach <bach.eric@gmail.com>
2023-05-21 21:19:02 -0600

    Test

--- frontend/src/pages/User/Bookings.tsx
+++ frontend/src/pages/User/Bookings.tsx
@@ -5,7 +5,7 @@ import { Container, Typography } from '@mui/material';
 
 import BookedAppointments from '../Booking/BookedAppointments';
 
-import aws_exports from '../../aws-exports';
+// import aws_exports from '../../aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 
 // Amplify.configure(aws_exports);
