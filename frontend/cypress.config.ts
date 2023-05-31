9ab60c117c5c98d9e7a50d28d34f2a41101c0b68
eric-bach <bach.eric@gmail.com>
2023-05-31 14:20:54 -0600

    Test

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -5,7 +5,13 @@ const defaultBaseUrl = 'http://localhost:3000';
 export default defineConfig({
   e2e: {
     baseUrl: defaultBaseUrl,
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      require('cypress-log-to-output').install(on);
+
+      config.baseUrl = config.env.url;
+
+      return config;
+    },
   },
   video: false,
 });
